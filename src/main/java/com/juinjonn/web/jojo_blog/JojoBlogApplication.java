a14d670d59900ab63c7d4d49f5c62c3333b293a2
juinjonn <juinjonn@126.com>
2018-08-11 00:08:37 +0800

    new project

---
+++ src/main/java/com/juinjonn/web/jojo_blog/JojoBlogApplication.java
@@ -0,0 +1,12 @@
+package com.juinjonn.web.jojo_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JojoBlogApplication.class, args);
+	}
+}
