09b14bcb4bdaf288c1c19fb38abf84c3bd2edae0
juinjonn <juinjonn@126.com>
2018-09-03 14:10:47 +0800

    a

---
+++ src/main/java/com/juinjonn/web/jojo_blog/controller/admin/AdminController.java
@@ -0,0 +1,8 @@
+package com.juinjonn.web.jojo_blog.controller.admin;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+    
+}
--- src/main/java/com/juinjonn/web/jojo_blog/controller/home/HomeController.java
+++ src/main/java/com/juinjonn/web/jojo_blog/controller/home/HomeController.java
@@ -1,14 +1,16 @@
 package com.juinjonn.web.jojo_blog.controller.home;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @RequestMapping("/")
     public String index(){
 
-        return "home";
+        return "love";
     }
 }
