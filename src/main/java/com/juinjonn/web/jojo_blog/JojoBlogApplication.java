b439037472498b033ea4f42d1ad5201525e95b7f
juinjonn <juinjonn@126.com>
2018-09-28 18:03:14 +0800

    add model

--- src/main/java/com/juinjonn/web/jojo_blog/JojoBlogApplication.java
+++ src/main/java/com/juinjonn/web/jojo_blog/JojoBlogApplication.java
@@ -2,9 +2,10 @@ package com.juinjonn.web.jojo_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JojoBlogApplication {
+public class JojoBlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JojoBlogApplication.class, args);
--- src/main/java/com/juinjonn/web/jojo_blog/modle/Blog.java
+++ src/main/java/com/juinjonn/web/jojo_blog/modle/Blog.java
@@ -2,12 +2,12 @@ package com.juinjonn.web.jojo_blog.modle;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 @Data
 public class Blog {
     private String title;
     private String context;
+    private String url;
 
 }
